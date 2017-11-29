5269c570aa1a0b1903362a7a1883749d933c0d87
mishapateyuk <mishapateyuk@gmail.com>
2017-11-29 20:42:24 +0300

    chore(build): add redux and scss loader to the project structure

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        Application will be here!
+      </div>
+    );
+  }
+}
+
+export default App;
